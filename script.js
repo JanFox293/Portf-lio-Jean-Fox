99e6251f6409ea14849c6069274bd475a65a6277
JanFox293 <jeafox@gmail.com>
2023-08-10 19:33:05 -0300

    Atualização strings JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend", "HTML", "CSS", "JS"],
+    strings: ["Frontend", "HTML", "CSS", "JS", "React"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
